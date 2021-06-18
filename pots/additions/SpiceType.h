44a04302099d60fbbed3cb919a9c5ec320c3724b
Priyanshu <upadhyaypriyanshu24@gmail.com>
2021-06-18 21:04:17 +0530

    additions enums

---
+++ pots/additions/SpiceType.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+ *  Copyright (C) 2020 by Nehal Sharma <lcs2020001@iiitl.ac.in>           *
+ *                                                                       *
+ *  This program is free software; you can redistribute it and/or        *
+ *  modify it under the terms of the GNU General Public License as       *
+ *  published by the Free Software Foundation; either version 3 of       *
+ *  the License, or (at your option) any later version.                  *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *************************************************************************/
+
+#ifndef HTCPCP_SPICETYPE_H
+#define HTCPCP_SPICETYPE_H
+
+
+enum class SpiceType { 
+    CINNAMON, NUTMEG, CARDAMOM, CLOVE, 
+};
+
+
+#endif //HTCPCP_SPICETYPE_H
---
+++ pots/additions/SweetenerType.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+ *  Copyright (C) 2020 by Nehal Sharma <lcs2020001@iiitl.ac.in>           *
+ *                                                                       *
+ *  This program is free software; you can redistribute it and/or        *
+ *  modify it under the terms of the GNU General Public License as       *
+ *  published by the Free Software Foundation; either version 3 of       *
+ *  the License, or (at your option) any later version.                  *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *************************************************************************/
+
+#ifndef HTCPCP_SWEETENERTYPE_H
+#define HTCPCP_SWEETENERTYPE_H
+
+
+enum class SweetenerType {
+    SUGAR, STEVIA, HONEY, MAPLE_SYRUP, AGAVE, 
+};
+
+
+#endif //HTCPCP_SWEETENERTYPE_H
