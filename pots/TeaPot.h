144f0f820540a8d85086b6c5d6de5bd93fa76337
Nehal Sharma <68962290+N-Shar-ma@users.noreply.github.com>
2021-07-09 10:50:38 -0700

    corrected indentation

--- pots/TeaPot.h
+++ pots/TeaPot.h
@@ -6,24 +6,24 @@
 class TeaPot : public Pot {
 protected:
     /**
-    * throws error 418
-    */
+     * throws error 418
+     */
     void addMilk(int type) override;
     /**
-    * throws error 418
-    */
+     * throws error 418
+     */
     void addSweetener(int type) override;
     /**
-    * throws error 418
-    */
+     * throws error 418
+     */
     void addSyrup(int type) override;
     /**
-    * throws error 418
-    */
+     * throws error 418
+     */
     void addSpice(int type) override;
     /**
-    * throws error 418
-    */
+     * throws error 418
+     */
     void addAlcohol(int type) override;
 public:
     TeaPot() {};
